33a8bd48891f21b9cd3cc5b7b2a049a3dcf22201
Salomão Machado <salomaomachadon@gmail.com>
2024-10-12 08:56:36 -0500

    createed new contract page

--- front_react/src/pages/NewEditContract/index.js
+++ front_react/src/pages/NewEditContract/index.js
@@ -7,11 +7,7 @@ import {
   ListItemText,
   TextField,
   Button,
-  Box,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
+  Box
 } from '@mui/material';
 import './styles.css'; // Importa o arquivo CSS
 
@@ -20,69 +16,70 @@ const NewEditContract = () => {
   const [contratantes, setContratantes] = useState([]);
 
   // Estado para os dados do documento
-  const [documento, setDocumento] = useState({
-    servico: '',    
-    honorarios: ''
+  const [contract, setContract] = useState({
+    name: '',
+    description: ''
   });
 
 
 
 
   // Função para lidar com a mudança dos inputs do documento
-  const handleDocumentoChange = (e) => {
+  const handleContractChange = (e) => {
     const { name, value } = e.target;
-    setDocumento({ ...documento, [name]: value });
+    setContract({ ...contract, [name]: value });
   };
 
   // Função para lidar com a submissão do formulário do documento
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Contratantes:', contratantes);
-    console.log('Documento:', documento);
+    // console.log('Contratantes:', contratantes);
+    console.log('Contrato:', contract);
   };
 
   return (
-    <Container className="container">
+    <Container className="container-contract">
       <Typography variant="h4" gutterBottom className="title">
         Novo Contrato
       </Typography>
 
-      {/* Lista de Dados dos Contratantes */}
-      <Typography variant="h6" gutterBottom className="subtitle">
-        Dados das partes
-      </Typography>
+
+
       {contratantes.length === 0 ? (
-        <Typography variant="body1" sx={{ color: 'gray' }}>
-          Não será possível gerar o documentos sem antes cadastrar as partes
-        </Typography>
+        <></>
       ) : (
-        <List className="list">
-          {contratantes.map((cont, index) => (
-            <ListItem key={index} className="list-item">
-              <ListItemText
-                primary={`Nome: ${cont.nome || 'Não informado'}`}
-                secondary={`CPF: ${cont.cpf || 'Não informado'}, Email: ${cont.email || 'Não informado'}, Telefone: ${cont.telefone || 'Não informado'}`}
-                className="list-item-text"
-              />
-            </ListItem>
-          ))}
-        </List>
+        <>
+          <Typography variant="h6" gutterBottom className="subtitle">
+            Dados das partes
+          </Typography>
+          <List className="list">
+            {contratantes.map((cont, index) => (
+              <ListItem key={index} className="list-item">
+                <ListItemText
+                  primary={`Nome: ${cont.nome || 'Não informado'}`}
+                  secondary={`CPF: ${cont.cpf || 'Não informado'}, Email: ${cont.email || 'Não informado'}, Telefone: ${cont.telefone || 'Não informado'}`}
+                  className="list-item-text"
+                />
+              </ListItem>
+            ))}
+          </List>
+        </>
       )}
 
 
-  
+
 
       {/* Formulário de Cadastro do Documento */}
       <Typography variant="h6" gutterBottom className="subtitle">
-        Dados do Documento
+        Dados do Contrato
       </Typography>
       <form onSubmit={handleSubmit}>
         <Box mb={2}>
           <TextField
-            label="Serviço"
-            name="servico"
-            value={documento.servico}
-            onChange={handleDocumentoChange}
+            label="Nome"
+            name="name"
+            value={contract.name}
+            onChange={handleContractChange}
             fullWidth
             required
             className="input-field"
@@ -90,17 +87,19 @@ const NewEditContract = () => {
         </Box>
         <Box mb={2}>
           <TextField
-            label="Honorários"
-            name="honorarios"
-            value={documento.honorarios}
-            onChange={handleDocumentoChange}
+            label="Descrição"
+            name="description"
+            value={contract.description}
+            onChange={handleContractChange}
             fullWidth
             required
+            multiline
+            rows={4}
             className="input-field"
           />
         </Box>
         <Button variant="contained" className="button-generate" type="submit">
-          Gerar
+          Gerar Contrato
         </Button>
       </form>
     </Container>
